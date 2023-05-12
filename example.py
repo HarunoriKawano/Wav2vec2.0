ad2abcbc21e62ebba1ae2dc22e8f3b3290b898e6
k-har <k.hnori0507@gmail.com>
2023-05-12 17:11:21 +0900

    update example

--- example.py
+++ example.py
@@ -29,26 +29,29 @@ class ExampleEncoder(nn.Module):
 
 
 if __name__ == '__main__':
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    print(f"Use device: {device}")
+
     extracted_feature_size = 562
     encoder_hidden_size = 1024
 
-    feature_extractor = ExampleFeatureExtractor(extracted_feature_size)
-    encoder = ExampleEncoder(extracted_feature_size, encoder_hidden_size)
+    feature_extractor = ExampleFeatureExtractor(extracted_feature_size).to(device)
+    encoder = ExampleEncoder(extracted_feature_size, encoder_hidden_size).to(device)
 
     # `(batch size, time steps, feature size)`
-    inputs = torch.randn(4, 1000, 80)
+    inputs = torch.randn(4, 1000, 80).to(device)
     # `(batch size)` Number of available time steps per batch
-    input_lengths = torch.tensor([1000, 871, 389, 487])
+    input_lengths = torch.tensor([1000, 871, 389, 487]).to(device)
 
     with open("config.json", "r", encoding="utf-8") as f:
         config = Wav2Vec2Config(**json.load(f))
 
-    model = Wav2Vec2Framework(config, feature_extractor, encoder)
-    compute_loss = Wav2vec2Loss(config)
+    model = Wav2Vec2Framework(config, feature_extractor, encoder).to(device)
+    criterion = Wav2vec2Loss(config)
 
     # tuple(Encoder hidden states with mask, Quantized features, Code book perplexity, Time mask indices)
     model_out = model(inputs, input_lengths)
-    loss = compute_loss(*model_out)
+    loss = criterion(*model_out)
 
     print(loss)
     loss.backward()
